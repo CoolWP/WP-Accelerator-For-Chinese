ce1847f00b5c5807e43573c2a4cea8bd52b4b483
Jack Lee <suifengtec@qq.com>
2016-02-03 04:25:56 +0800

    typo checked.

--- wp-accelerator-for-chinese.php
+++ wp-accelerator-for-chinese.php
@@ -14,9 +14,7 @@
  * License: GPLv2
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
-
 defined('ABSPATH') || exit;
-
 if (!class_exists('WP_AcceleratorForChinese')) {
 
 	class WP_AcceleratorForChinese {
